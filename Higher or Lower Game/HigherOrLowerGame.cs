8c7014a78a67f177f7261a6b9a9bd799f88eaadc
LucasB-07 <99080017@mydavinci.nl>
2025-11-25 09:34:43 +0100

    Update HigherOrLowerGame Intro
    - Added an intro
    - Added Temp Comments

--- Higher or Lower Game/HigherOrLowerGame.cs
+++ Higher or Lower Game/HigherOrLowerGame.cs
@@ -13,7 +13,24 @@ namespace HigherOrLowerGame
 
             Console.Title = "Higher or Lower Game";
             Console.WriteLine("< - - - - - - - - 📈 Higher or Lower Game 📉 - - - - - - - - >");
+            Thread.Sleep(1000);
+            Console.WriteLine("Welcome to the Higher or Lower Game! Below is some information: ");
+            Thread.Sleep(1000);
+            Console.WriteLine("The computer picks a random number between 1 and 100.");
+            Thread.Sleep(1000);
+            Console.WriteLine("You must guess the number. After each guess, the computer will tell you whether you need to go higher or lower.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Try to guess the number in as few attempts as possible! Good luck and have fun. \n");
+            Thread.Sleep(1000);
+            Console.WriteLine("< - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - >");
 
+            //TODO: While true loop to play again + generate random number
+
+            //TODO: While not correct at guessed, ask question + hidden test trigger + validate input between 1-100 + attempts + hints + error handling
+
+            //TODO: Congratulate + show attempts + play again prompt
+
+            //TODO: Play again?
         }
     }
 }
