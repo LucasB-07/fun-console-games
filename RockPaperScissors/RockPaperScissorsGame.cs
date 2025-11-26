1e77bc6b1b0c3230b153d73510adf27a54548c08
LucasB-07 <99080017@mydavinci.nl>
2025-11-26 15:03:24 +0100

    Update RockPaperScissorsGame

--- RockPaperScissors/RockPaperScissorsGame.cs
+++ RockPaperScissors/RockPaperScissorsGame.cs
@@ -11,11 +11,20 @@ namespace RockPaperScissors
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-            Console.Title = "RockPaperScissors";
+            Console.Title = "Rock Paper Scissors";
 
             Console.WriteLine("< - - - - - - - - ✊ Rock Paper Scissors ✋ - - - - - - - - >");
             Thread.Sleep(1000);
-            Console.WriteLine("Welcome to the Rock Paper Scissors Game!");
+            Console.WriteLine("Welcome to the Rock Paper Scissors Game! Below is some information: ");
+            Thread.Sleep(1000);
+            Console.WriteLine("You will be playing against the computer.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Choose Rock, Paper, or Scissors to play. Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Try to win as many rounds as possible! Good luck and have fun. \n");
+            Thread.Sleep(1000);
+
+            
         }
     }
 }
