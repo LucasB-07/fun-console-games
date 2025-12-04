8520d9ec3379f60cd523f90f658f831082ebf13e
LucasB-07 <99080017@mydavinci.nl>
2025-12-04 13:46:14 +0100

    Update PasswordGenerator Added Basic Stuff
    - Added some basic stuff to let everything work properly

--- PasswordGenerator/PasswordGenerator.cs
+++ PasswordGenerator/PasswordGenerator.cs
@@ -7,6 +7,11 @@ class PasswordGenerator
 {
     static void Main()
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+        Console.Title = "Password Generator";
+
 
     }
 }
