f032e36740a454ebcbf82130c37589b1bbb34a99
LucasB-07 <99080017@mydavinci.nl>
2025-12-05 12:03:49 +0100

    Update PasswordGenerator EDUCATIONAL PURPOSES ONLY
    - Changed some text to be even more clearly

--- PasswordGenerator/PasswordGenerator.cs
+++ PasswordGenerator/PasswordGenerator.cs
@@ -1,7 +1,7 @@
 // DISCLAIMER:
 // This project is intended solely for educational and developmental purposes.
 // It is not designed or suitable for real-world, production, operational or security-critical use.
-// The author accepts no responsibility or liability for any consequences arising
+// The author accepts no responsibility or liability for any consequences arising from misuse.
 // MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.
 using System;
 using System.Threading;
