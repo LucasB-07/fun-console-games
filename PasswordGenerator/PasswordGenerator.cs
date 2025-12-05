561b2bad62fc35e3897b12c4f1f9a2b0b17adf37
LucasB-07 <99080017@mydavinci.nl>
2025-12-05 12:06:03 +0100

    Update PasswordGenerator EDUCATIONAL PURPOSES ONLY
    - Changed some text to be even more clearly

--- PasswordGenerator/PasswordGenerator.cs
+++ PasswordGenerator/PasswordGenerator.cs
@@ -25,7 +25,7 @@ class PasswordGenerator
         Console.WriteLine("This application generates a random password based on the length you specify.");
         Thread.Sleep(1000);
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("This project is intended solely for educational and developmental purposes. It is not designed or suitable for real-world, production, operational or security-critical use. The author accepts no responsibility or liability for any consequences arising from the use of this software. MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.\n");
+        Console.WriteLine("This project is intended solely for educational and developmental purposes. It is not designed or suitable for real-world, production, operational or security-critical use. The author accepts no responsibility or liability for any consequences arising from misuse. MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.\n");
         Console.ResetColor();
         Thread.Sleep(1000);
         Console.WriteLine("< - - - - - - - - - - - - - - - - - - - - - - - - - - - - - >");
