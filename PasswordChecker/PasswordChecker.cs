da1dbe5467005f4cdb7bde71959030d3a4743b92
LucasB-07 <99080017@mydavinci.nl>
2025-12-08 14:56:15 +0100

    Update PasswordChecker Added comments + using Systems
    - Added Comments
    - Added Using Systems

---
+++ PasswordChecker/PasswordChecker.cs
@@ -0,0 +1,9 @@
+// DISCLAIMER:
+// This project is intended solely for educational and developmental purposes.
+// It is not designed or suitable for real-world, production, operational or security-critical use.
+// The author accepts no responsibility or liability for any consequences arising from misuse.
+// MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
