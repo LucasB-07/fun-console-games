5993195fcfca8341996c052fedd5bc3b10019c71
LucasB-07 <99080017@mydavinci.nl>
2025-12-08 15:07:38 +0100

    Update PasswordChecker Added (see desc)
    - Added UTF8
    - Added Console.Title
    - Added prints

--- PasswordChecker/PasswordChecker.cs
+++ PasswordChecker/PasswordChecker.cs
@@ -7,3 +7,25 @@ using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+
+class PasswordChecker
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Title = "Password Checker";
+
+        Console.WriteLine("< - - - - - - - - 🔐 Password Checker 🔐 - - - - - - - - >");
+        Thread.Sleep(1000);
+        Console.WriteLine("Welcome to the Password Checker! Below is some information: ");
+        Thread.Sleep(1000);
+        Console.WriteLine("This application checks the strength of your password based on various criteria.");
+        Thread.Sleep(1000);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("This project is intended solely for educational and developmental purposes. It is not designed or suitable for real-world, production, operational or security-critical use. The author accepts no responsibility or liability for any consequences arising from misuse. MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.\n");
+        Console.ResetColor();
+        Thread.Sleep(1000);
+        Console.WriteLine("< - - - - - - - - 🔐 Password Checker 🔐 - - - - - - - - >");
+    }
+}
