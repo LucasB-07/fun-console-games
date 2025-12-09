7f2e69f56a7cca82cf5d788b19c59a60f5e433a1
LucasB-07 <99080017@mydavinci.nl>
2025-12-09 12:12:49 +0100

    Update RockPaperScissorsGame Added short answer for yes & no
    - Added short answer for yes & no

--- Rock_Paper_Scissors_Game/RockPaperScissorsGame.cs
+++ Rock_Paper_Scissors_Game/RockPaperScissorsGame.cs
@@ -204,11 +204,11 @@ namespace RockPaperScissors
                     Console.Write("Do you want to play again? (yes/no): ");
                     var answer = Console.ReadLine().ToLower();
 
-                    if (answer == "yes")
+                    if (answer == "yes" || answer == "y")
                     {
                         break;
                     }
-                    else if (answer == "no")
+                    else if (answer == "no" || answer == "n")
                     {
                         playAgain = false;
                         break;
