27f7ccc8bbcdeed3067e6e10476c3960446bbef2
LucasB-07 <99080017@mydavinci.nl>
2025-12-09 12:07:47 +0100

    Update DiceGame Added short answer for yes & no
    - Added short answer for yes & no

--- Dice_Game/DiceGame.cs
+++ Dice_Game/DiceGame.cs
@@ -163,11 +163,11 @@ namespace DiceGame
                     Console.Write("Do you want to play again? (yes/no): ");
                     var answer = Console.ReadLine().ToLower();
 
-                    if (answer == "yes")
+                    if (answer == "yes" || answer == "y")
                     {
                         break;
                     }
-                    else if (answer == "no")
+                    else if (answer == "no" || answer == "n")
                     {
                         playAgain = false;
                         break;
