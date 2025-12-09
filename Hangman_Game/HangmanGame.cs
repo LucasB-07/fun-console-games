1b174bf7a15fdcc72b4aed0c226305509b7fad51
LucasB-07 <99080017@mydavinci.nl>
2025-12-09 12:14:15 +0100

    Update HangmanGame Added short answer for yes & no
    - Added short answer for yes & no

--- Hangman_Game/HangmanGame.cs
+++ Hangman_Game/HangmanGame.cs
@@ -111,11 +111,11 @@ namespace HangmanGame
                     Console.Write("Do you want to play again? (yes/no):");
                     var answer = Console.ReadLine().ToLower().Trim();
 
-                    if (answer == "yes")
+                    if (answer == "yes" || answer == "y")
                     {
                         break;
                     }
-                    else if (answer == "no")
+                    else if (answer == "no" || answer == "n")
                     {
                         playAgain = false;
                         break;
