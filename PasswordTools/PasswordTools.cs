44d425307751991b9c98643159df47620820f408
LucasB-07 <99080017@mydavinci.nl>
2025-12-11 11:16:57 +0100

    Created PasswordTools
    - Created a new era

---
+++ PasswordTools/PasswordTools.cs
@@ -0,0 +1,5 @@
+// DISCLAIMER:
+// This project is intended solely for educational and developmental purposes.
+// It is not designed or suitable for real-world, production, operational or security-critical use.
+// The author accepts no responsibility or liability for any consequences arising from misuse.
+// MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.
