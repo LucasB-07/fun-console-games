b5dc7aec716d30cac20243200490b1341123d8f5
LucasB-07 <99080017@mydavinci.nl>
2025-12-11 11:20:17 +0100

    Update PasswordTools Added Using Systems
    - Added Using Systems

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -3,3 +3,11 @@
 // It is not designed or suitable for real-world, production, operational or security-critical use.
 // The author accepts no responsibility or liability for any consequences arising from misuse.
 // MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.
+
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Globalization;
+using System.Security.Cryptography;
+
