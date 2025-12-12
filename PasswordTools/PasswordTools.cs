47366f471286e31aad7551f08cb8c4fec5a21dd5
LucasB-07 <99080017@mydavinci.nl>
2025-12-12 09:08:05 +0100

    Update PasswordTools Added full if statement for Uppercase Letter Check
    - Added full if statement for Uppercase Letter Check

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -202,6 +202,18 @@ class PasswordTools
                 feedback += "❌ | Contains poor length (<8 characters). Use a longer password.\n";
             }
 
+            //2. Uppercase Letters
+            if (Regex.IsMatch(password, @"[A-Z]"))
+            {
+                score += 15;
+                if (Regex.IsMatch(password, @"(?=.*[A-Z].*[A-Z])"))
+                {
+                    score += 5;
+                    feedback += "✅ | Contains multiple uppercase letters.\n";
+                }
+                feedback += "✅ | Contains uppercase letter.\n";
+            }
+
         }
     }
 }
