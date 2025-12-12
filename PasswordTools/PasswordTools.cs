4639c3f03f3c805a4adf4503278cdd69b0fd0b8c
LucasB-07 <99080017@mydavinci.nl>
2025-12-12 08:52:07 +0100

    Update PasswordTools Added Intro
    - Added Intro

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -152,5 +152,19 @@ class PasswordTools
     static void RunPasswordChecker()
     {
         Console.Title = "Password Checker";
+
+        Console.WriteLine("< - - - - - - - - 🔐 Password Checker 🔐 - - - - - - - - >");
+        Thread.Sleep(1000);
+        Console.WriteLine("Welcome to the Password Checker! Below is some information: ");
+        Thread.Sleep(1000);
+        Console.WriteLine("This application checks the strength of your password based on various criteria.");
+        Thread.Sleep(1000);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("This project is intended solely for educational and developmental purposes. It is not designed or suitable for real-world, production, operational or security-critical use. The author accepts no responsibility or liability for any consequences arising from misuse. MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.\n");
+        Console.ResetColor();
+        Thread.Sleep(1000);
+        Console.WriteLine("< - - - - - - - - - - - - - - - - - - -  - - - - - - - - >");
+
+        
     }
 }
