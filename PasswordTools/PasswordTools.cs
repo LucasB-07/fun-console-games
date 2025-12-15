4c691bc3b72e2fd631377731fbff00f059ee1ceb
LucasB-07 <99080017@mydavinci.nl>
2025-12-15 16:39:54 +0100

    Update PasswordTools Added content to Special Char Check
    - Added content to Special Char Check

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -287,8 +287,10 @@ class PasswordTools
             }
             else if (Regex.IsMatch(password, @"[\W_]"))
             {
-                
+                score += 15;
+                feedback += "✅ | Contains special character.\n";
             }
+
 
             //6. Common Patterns
             if (Regex.IsMatch(password, @"(123456|password|qwerty|!@#$%^&*()_|letmein|welcome|admin|iloveyou|token|secret|wasd|abc123|111111|123123|password123|lol|lol123|admin123|zxcvbn)", RegexOptions.IgnoreCase))
