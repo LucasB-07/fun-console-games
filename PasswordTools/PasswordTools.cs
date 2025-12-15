c23738aba902fe1d3e7504cc9b629e49c878d8c8
LucasB-07 <99080017@mydavinci.nl>
2025-12-15 16:07:46 +0100

    Update PasswordTools Removed Old Check
    - Removed Old Check

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -246,22 +246,6 @@ class PasswordTools
                 feedback += "❌ | No lowercase letters. Consider adding some.\n";
             }
 
-            // //4. Digits
-            // if (Regex.IsMatch(password, @"\d"))
-            // {
-            //     score += 15;
-            //     if (Regex.IsMatch(password, @"(?=(.*\d){2,})"))
-            //     {
-            //         score += 5;
-            //         feedback += "✅ | Contains multiple digits.\n";
-            //     }
-            //     feedback += "✅ | Contains digit.\n";
-            // }
-            // else
-            // {
-            //     feedback += "❌ | No digits. Consider adding some.\n";
-            // }
-
             //TODO: update the Digits to be optional in the next version
             if (Regex.IsMatch(password, @"(?=.*\d){2,}"))
             {
