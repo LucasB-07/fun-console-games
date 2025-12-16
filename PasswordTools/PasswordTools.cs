f2edb23db554782ca8856edcea30a9614b35b18d
LucasB-07 <99080017@mydavinci.nl>
2025-12-16 11:54:50 +0100

    Update PasswordTools Added content to for loop
    - Added content to for loop

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -47,6 +47,9 @@ class PasswordTools
                 
                 for ( int j = 0; j < 13; j += 1)
                 {
+                    Thread.Sleep(200);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(".");
                 }
 
                 RunPasswordGenerator();
