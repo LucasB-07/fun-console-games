283f446bbd7e50832595024e33be5d926f297149
LucasB-07 <99080017@mydavinci.nl>
2025-12-16 12:08:04 +0100

    Update PasswordTools Removed \n
    - Removed \n

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -180,7 +180,7 @@ class PasswordTools
         Console.WriteLine("This application checks the strength of your password based on various criteria.");
         Thread.Sleep(1000);
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("This project is intended solely for educational and developmental purposes. It is not designed or suitable for real-world, production, operational or security-critical use. The author accepts no responsibility or liability for any consequences arising from misuse. MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.\n");
+        Console.WriteLine("This project is intended solely for educational and developmental purposes. It is not designed or suitable for real-world, production, operational or security-critical use. The author accepts no responsibility or liability for any consequences arising from misuse. MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.");
         Console.ResetColor();
         Thread.Sleep(1000);
         Console.WriteLine("< - - - - - - - - - - - - - - - - - - -  - - - - - - - - >");
