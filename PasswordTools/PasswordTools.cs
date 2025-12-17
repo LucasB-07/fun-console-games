6ccb49c0179390f0064e6a74b9adb99c278c4c9d
LucasB-07 <99080017@mydavinci.nl>
2025-12-17 14:59:20 +0100

    Update PasswordTools Removed Dashes
    - Removed Dashes

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -232,7 +232,7 @@ class PasswordTools
         Console.WriteLine("This project is intended solely for educational and developmental purposes. It is not designed or suitable for real-world, production, operational or security-critical use. The author accepts no responsibility or liability for any consequences arising from misuse. MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.");
         Console.ResetColor();
         Thread.Sleep(1000);
-        Console.WriteLine("< - - - - - - - - - - - - - - - - - - -  - - - - - - - - >");
+        Console.WriteLine("< - - - - - - - - - - - - - - - - - - - - - - - - - - >");
 
         bool checkAgain = true;
         while (checkAgain)
