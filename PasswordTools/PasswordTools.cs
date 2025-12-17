f9a46255321f75a650ea20e323bc87d47b261f7f
LucasB-07 <99080017@mydavinci.nl>
2025-12-17 11:36:20 +0100

    Update PasswordTools Added Content
    - Added Content

--- PasswordTools/PasswordTools.cs
+++ PasswordTools/PasswordTools.cs
@@ -102,8 +102,11 @@ class PasswordTools
 
                     for ( int k = 0; k < 13; k += 1)
                     {
-                        
+                        Thread.Sleep(200);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(".");
                     }
+
             }
         }
         Console.WriteLine("❤️ Made With Love By LucasB-07 ❤️\n");
