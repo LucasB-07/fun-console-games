08d9585b406ffc6454bad3575115c97eef780355
LucasB-07 <99080017@mydavinci.nl>
2025-12-18 11:36:05 +0100

    Update PasswordToolsOOP Added Using System
    - Added Using System

--- PasswordTools/PasswordToolsOOP.cs
+++ PasswordTools/PasswordToolsOOP.cs
@@ -4,3 +4,10 @@
 // The author accepts no responsibility or liability for any consequences arising from misuse.
 // MUST NOT BE USED IN PRODUCTION OR SECURITY-CRITICAL SYSTEMS.
 
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Globalization;
+using System.Security.Cryptography;
+
